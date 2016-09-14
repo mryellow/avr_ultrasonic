e8124fb590163eb0b5e227988aaf03abda73f3e1
Mr-Yellow <mr-yellow@mr-yellow.com>
2016-09-14 14:56:27 +1000

    Use array for pulse measuing in prep for multi

--- ultrasonic.c
+++ ultrasonic.c
@@ -22,37 +22,40 @@ uint8_t I2C_buffer[SENSOR_NUM];
 #define I2C_SLAVE_ADDRESS 0x10
 void handle_I2C_interrupt(volatile uint8_t TWI_match_addr, uint8_t status);
 
-static volatile uint8_t pulse_length = 0;
-static volatile uint8_t result = 0;
+static volatile uint8_t pulse_length[SENSOR_NUM];
 
 // Measurement Timer Interrupt
 // Counts in centimetres
 ISR(TIMER2_COMPA_vect) {
-  // TODO: Increment array contents
-  pulse_length++;
+  for (x=0;x<SENSOR_NUM;x++) {
+    pulse_length[x]++;
+  }
 }
 
 // Echo Input Interrupt
 // Measures echo length
 // TODO: Macro to define one for each sensor
 ISR(PCINT0_vect) {
+  // TODO: Detect `changedbits` in each port
   //uint8_t changedbits;
   //changedbits = PINB ^ portbhistory;
   //portbhistory = PINB;
   //if(changedbits & (1 << PINB0))
 
+  // TODO: Detect array of pins from `changedbits`
   // high
   if (PINB & _BV(ECHO_PIN)) {  // Pulse start
     // Leaving timer running for other sensors
     //TCNT1 = 0;
-    pulse_length = 0;
+    pulse_length[0] = 0;
   // low
   } else { // Pulse end
-    I2C_buffer[0] = pulse_length;
+    I2C_buffer[0] = pulse_length[0];
   }
 }
 
 void sensor_setup(void) {
+  // TODO: Configure array of pins
   DDRB |= _BV(TRIG_PIN);    // Trig pin is output
   PORTB &= ~_BV(TRIG_PIN);  // Set Trig to 0
   DDRB &= ~_BV(ECHO_PIN);   // Sensor pin is input
@@ -63,6 +66,7 @@ void sensor_setup(void) {
   TCCR2A = _BV(COM2A1) | _BV(WGM21); // Reset OC2A, CTC Mode
   TCCR2B = _BV(CS21); // Clock = ClkI/O / 8
   //OCR1C = US_PER_CM - 1;
+  // TODO: Is `US_PER_CM` correct?
   OCR2A = US_PER_CM - 1;
   //TIMSK |= _BV(OCIE1A); // Enable Interrupt TimerCounter1 Compare Match A
   TIMSK2 |= _BV(OCIE2A); // Enable Interrupt TimerCounter2 Compare Match A
@@ -107,6 +111,7 @@ int main(void) {
 
   // TODO: Timer which triggers sensors in-turn, times them out too.. Use a timer?
   while(1) {
+    // TODO: Trigger each pin in array
     trigger();
 
     // Wait for echo
